06bf236ac99a43e25de8c05a1d234f46299485d7
Christian Gil <mrcgam.christian@gmail.com>
2024-02-02 12:39:24 -0600

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+import { ready, subscribe, unsubscribe } from '@payloadcms/live-preview';
+import { type Ref, ref, watchEffect } from 'vue';
+
+export const useLivePreview = <T>(props: {
+  apiRoute?: string;
+  depth?: number;
+  initialData: T;
+  serverURL: string;
+}): { data: Ref<T>; isLoading: Ref<boolean> } => {
+  const { apiRoute, depth, initialData, serverURL } = props;
+  const data = ref(initialData) as Ref<T>; // Workaround for weird Ref<T> behavior
+  const isLoading = ref(true);
+  const hasSentReadyMessage = ref(false);
+
+  const onChange = (mergedData: T) => {
+    data.value = mergedData;
+    isLoading.value = false;
+  };
+
+  watchEffect((onCleanup) => {
+    const subscription = subscribe({
+      apiRoute,
+      callback: onChange,
+      depth,
+      initialData,
+      serverURL,
+    });
+
+    if (!hasSentReadyMessage.value) {
+      hasSentReadyMessage.value = true;
+      ready({ serverURL });
+    }
+
+    onCleanup(() => {
+      unsubscribe(subscription);
+    });
+  });
+
+  return { data, isLoading };
+};
